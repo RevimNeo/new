6cc66b2929c50d5e2052c6ac6a50bbb4f5033cde
Rainygan <neorevim@gmail.com>
2025-04-04 23:56:07 +0800

    "Fixed" lua_ls loop.
    maybe fixed lua_ls looping 10k files.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,14 +2,14 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim"
+    "williamboman/mason-lspconfig.nvim",
   },
 
-  config = function ()
-    local lsp_config = require("lspconfig")
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local blink_cmp = require("blink.cmp")
+  config = function()
+    local lsp_config = require "lspconfig"
+    local mason = require "mason"
+    local mason_lspconfig = require "mason-lspconfig"
+    local blink_cmp = require "blink.cmp"
 
     local capabilities = blink_cmp.get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -17,42 +17,61 @@ return {
       local opts = {
         noremap = true,
         silent = true,
-        buffer = bufnr
+        buffer = bufnr,
       }
 
       local map = vim.keymap.set
 
-			map("n", "gd", vim.lsp.buf.definition, opts)
-			map("n", "gr", vim.lsp.buf.references, opts)
-			map("n", "K", vim.lsp.buf.hover, opts)
-			map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-			map("n", "<leader>rn", vim.lsp.buf.rename, opts)
-			map("n", "<leader>e", vim.diagnostic.open_float, opts)
-			map("n", "[d", vim.diagnostic.goto_prev, opts)
-			map("n", "]d", vim.diagnostic.goto_next, opts)
+      map("n", "gd", vim.lsp.buf.definition, opts)
+      map("n", "gr", vim.lsp.buf.references, opts)
+      map("n", "K", vim.lsp.buf.hover, opts)
+      map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      map("n", "<leader>e", vim.diagnostic.open_float, opts)
+      map("n", "[d", vim.diagnostic.goto_prev, opts)
+      map("n", "]d", vim.diagnostic.goto_next, opts)
     end
 
-    mason.setup({
+    mason.setup {
       ui = {
         icons = {
           package_installed = "✓",
           package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
-    })
+          package_uninstalled = "✗",
+        },
+      },
+    }
 
-    mason_lspconfig.setup({
-      ensure_installed = { "lua_ls", "ts_ls", "emmet_language_server", "eslint" }
-    })
+    mason_lspconfig.setup {
+      ensure_installed = { "lua_ls", "ts_ls", "emmet_language_server", "eslint", "pyright" },
+    }
 
-    mason_lspconfig.setup_handlers({
+    mason_lspconfig.setup_handlers {
       function(server_name)
-        lsp_config[server_name].setup({
+        local opts = {
           on_attach = on_attach,
-          capabilities = capabilities
-        })
-      end
-    })
-  end
+          capabilities = capabilities,
+        }
+        if server_name == "lua_ls" then
+          table.insert(opts, {
+            settings = {
+              Lua = {
+                runtime = { version = "LuaJIT" },
+                diagnostics = { globals = { "vim" } },
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                  checkThirdParty = false,
+                },
+                telemetry = { enable = false },
+              },
+            },
+            root_dir = function(fname)
+              return lspconfig.util.find_git_ancestor(fname) or lspconfig.util.path.dirname(fname)
+            end,
+          })
+        end
+        lsp_config[server_name].setup(opts)
+      end,
+    }
+  end,
 }
