119eb996db7238b13d73a9e85b7e656acf080431
Rainygan <neorevim@gmail.com>
2025-04-07 14:40:26 +0800

    Added noice.nvim | Cmdline
    Added noice.nvim for a sleek command line, tweaked blink-cmp cmd
    autocompletion.

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -32,6 +32,38 @@ return {
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono'
     },
+    cmdline = {
+      enabled = true,
+      -- use 'inherit' to inherit mappings from top level `keymap` config
+      keymap = { preset = 'cmdline' },
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Search forward and backward
+        if type == '/' or type == '?' then return { 'buffer' } end
+        -- Commands
+        if type == ':' or type == '@' then return { 'cmdline' } end
+        return {}
+      end,
+    
+      completion = {
+        trigger = {
+          show_on_blocked_trigger_characters = {},
+          show_on_x_blocked_trigger_characters = {},
+        },
+        list = {
+          selection = {
+            -- When `true`, will automatically select the first item in the completion list
+            preselect = true,
+            -- When `true`, inserts the completion item automatically when selecting it
+            auto_insert = true,
+          },
+        },
+        -- Whether to automatically show the window when new completion items are available
+        menu = { auto_show = true },
+        -- Displays a preview of the selected item on the current line
+        ghost_text = { enabled = true }
+      }
+    },
 
     -- (Default) Only show the documentation popup when manually triggered
     completion = { documentation = { auto_show = false } },
