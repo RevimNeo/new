77051094bd93d495bad9cda3aac293d3360ea859
Rainygan <neorevim@gmail.com>
2025-04-07 09:23:41 +0800

    Added mini.notify
    I added a notification demo so that the system won't bug you with the
    prompt to continue or insert a command.
    
    This will also make debugging a bit better and make your workflow easier
    because there won't be an error that will ruin your flow.

---
+++ lua/plugins/mini-notify.lua
@@ -0,0 +1,20 @@
+return {
+  "echasnovski/mini.notify",
+  version = '*',
+  config = function ()
+    local notify = require('mini.notify')
+    notify.setup({
+      window = {
+        winblend = 0
+      }
+    })
+
+    vim.notify = notify.make_notify({
+      ERROR = { duration = 5000 },
+      WARN = { duration = 4000 },
+      INFO = { duration = 3000 }
+    })
+
+    vim.notify('Notification daemon loaded.', vim.log.levels.INFO)
+  end
+}
